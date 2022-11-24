d4fabd8250013d5c72b0819172453fcad6225b60
ArthurHab <arthur200106@gmail.com>
2022-11-23 22:12:44 -0300

    create main

--- pages/components/Modal.tsx
+++ pages/components/Modal.tsx
@@ -1,8 +1,12 @@
-export function Modal() {
+interface PropsTypes {
+    title: string;
+    paragrap: string;
+}
+export function Modal(props: PropsTypes){
     return (
         <div>
-            <h1>Modal</h1>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe possimus reiciendis similique cum. Voluptatibus, deleniti molestiae accusamus quisquam dolore illum molestias possimus dolorum, laudantium aspernatur iste magni quis, vero voluptatum?</p>
+            <h1>{props.title}</h1>
+            <p>A {props.paragrap} tem melões grandes,</p>
         </div>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,18 @@
+import { useState } from "react"
 import { Modal } from "./components/Modal"
-import { useState } from "react";
+import styles from "./home.module.scss"
+
 export default function Home() {
-  let [open, setOpen] = useState(false)
-  function abrirModal(){
+  let [open,setOpen] = useState(false)
+  function openModal() {
     setOpen(
       open = !open
     )
   }
-
   return (
-    <div>
-      {open ? <Modal /> : null}
-      <button onClick={abrirModal} >Abrir Modal</button>
-      
-    </div>
-    
+    <main className={styles.contentContainer}>
+      <div className="text">Teste</div>
+      <img src="" alt="" />
+    </main>
   )
 }
